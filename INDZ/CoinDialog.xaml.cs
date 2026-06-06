ebac23649f3f45d5c046894b6ed5b34ea4fe38ed
Impartialll <maxutkom@gmail.com>
2026-06-06 20:24:14 +0300

    Add project files.

---
+++ INDZ/CoinDialog.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+
+namespace INDZ
+{
+    public partial class CoinDialog : Window
+    {
+        private Action<int, int> _updateStatsCallback;
+        private Random _random = new Random();
+
+        public string LastInput { get; private set; }
+
+        public CoinDialog(string lastInputValue, Action<int, int> updateStatsCallback)
+        {
+            InitializeComponent();
+            _updateStatsCallback = updateStatsCallback;
+            LastInput = lastInputValue;
+            txtTosses.Text = lastInputValue; 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtTosses.Focus();
+            txtTosses.SelectAll();
+        }
+
+        private bool ProcessTosses()
+        {
+            if (int.TryParse(txtTosses.Text, out int n) && n > 0)
+            {
+                LastInput = txtTosses.Text; 
+                int headsCount = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (_random.Next(2) == 1)
+                    {
+                        headsCount++;
+                    }
+                }
+
+                _updateStatsCallback?.Invoke(n, headsCount);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, введіть ціле додатне число.", "Помилка вводу", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtTosses.Focus();
+                txtTosses.SelectAll();
+                return false;
+            }
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProcessTosses())
+            {
+                this.DialogResult = true; 
+            }
+        }
+
+        private void BtnApply_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProcessTosses())
+            {
+                txtTosses.Focus();
+                txtTosses.SelectAll();
+            }
+        }
+    }
+}
---
+++ INDZ/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INDZ
+{
+    public partial class MainWindow : Window
+    {
+        private int _totalTosses = 0;
+        private int _totalHeads = 0;
+
+        private string _lastInputValue = "10";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpenDialog_Click(object sender, RoutedEventArgs e)
+        {
+            CoinDialog dialog = new CoinDialog(_lastInputValue, UpdateStats)
+            {
+                Owner = this 
+            };
+
+            dialog.ShowDialog(); 
+
+            _lastInputValue = dialog.LastInput;
+        }
+        private void UpdateStats(int tossesCount, int headsCount)
+        {
+            _totalTosses += tossesCount;
+            _totalHeads += headsCount;
+
+            double percent = _totalTosses == 0 ? 0 : ((double)_totalHeads / _totalTosses) * 100;
+
+            tbTotalTosses.Text = $"Сумарне число підкидань = {_totalTosses}";
+            tbHeadsPercent.Text = $"Відсоток орлів = {Math.Round(percent, 2)}%";
+        }
+    }
+}
